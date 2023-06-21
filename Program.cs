c6cfe50e4c752a6ed3d7b987899d73c8c3619608
napalmtorchx <gamemakermagician@gmail.com>
2023-06-21 13:28:42 -0400

    First commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace OSMaker
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Debug.Info("OSMaker Utility v1.0");
+        }
+    }
+}
